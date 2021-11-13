4ab76eca0d011fea596bf0bb4d26cc8c89383def
ShekAdnan <adnansk418@gmail.com>
2021-11-13 14:24:42 +0600

    my first app

---
+++ myapp/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //MAIN FUNCTION
+  runApp(// RUNAPP PROPERTY
+      Center(
+          //WIDGET
+          child: (Text(
+    //TEXT WIDGET
+    'This is my first flutter application',
+    textDirection: TextDirection.ltr, //TEXT PROPERTY
+  ))));
+}
