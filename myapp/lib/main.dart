31021b1ee16f2cafe4967501e4635cfb5b2afcc1
ShekAdnan <adnansk418@gmail.com>
2021-11-13 15:16:39 +0600

    stateless widget

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,13 +1,39 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   //MAIN FUNCTION
+//   runApp(// RUNAPP PROPERTY
+//       Center(
+//           //WIDGET
+//           child: (Text(
+//     //TEXT WIDGET
+//     'This is my first flutter application',
+//     textDirection: TextDirection.ltr, //TEXT PROPERTY
+//   ))));
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
-  //MAIN FUNCTION
-  runApp(// RUNAPP PROPERTY
-      Center(
-          //WIDGET
-          child: (Text(
-    //TEXT WIDGET
-    'This is my first flutter application',
-    textDirection: TextDirection.ltr, //TEXT PROPERTY
-  ))));
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'stateless widget',
+      home: Scaffold(
+        body: Container(
+          color: Colors.pink,
+          child: Container(
+            color: Colors.yellow,
+            margin: const EdgeInsets.all(30.0),
+          ),
+        ),
+      ),
+    );
+  }
 }
