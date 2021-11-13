784e938d8a02f158e7b4f543a8b69e338241e961
ShekAdnan <adnansk418@gmail.com>
2021-11-13 17:13:32 +0600

    statefull widget

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -12,28 +12,78 @@
 //   ))));
 // }
 
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(Application()); //CLASS CALLED
+// }
+
+// class Application extends StatelessWidget { //STATELEESS WIDGET
+//   const Application({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'stateless widget', //STRING
+//       home: Scaffold( //SCAFFOLD WIDGET
+//         body: Container( //CONTAINER WIDGET
+//           color: Colors.pink, //COLORS WIDGET
+//           child: Container( //CONTAINER WIDGET
+//             color: Colors.yellow, //COLORS WIDGET
+//             margin: const EdgeInsets.all(30.0), //EDGEINSETS WIDDGET
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Application());
+  runApp(App()); //CLASS NAME APP
 }
 
-class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  //STATEFUL WEIDGET
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  String ttext = ''; //STRING TYPE VARIABLE DECLARED TTEXT
+  @override
+  void initState() {
+    // INIT STATE
+    ttext = 'click on me'; //VALUE ASSIGNED INSIDE OF STRING TYPE VARRIABLE
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    ttext = 'I CHANGED IT';
+    // TODO: implement setState
+    super.setState(fn);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'stateless widget',
-      home: Scaffold(
-        body: Container(
-          color: Colors.pink,
-          child: Container(
-            color: Colors.yellow,
-            margin: const EdgeInsets.all(30.0),
+        title: 'stateful widget',
+        home: Scaffold(
+            body: Center(
+          child: ElevatedButton(
+            // ELEVATED BUTTON
+            onPressed: () {
+              //ONPRESSED FUNCTION
+              setState(
+                  () {}); //SETSTATE FUNCTION WAS CALLLEDD INSIDE ON PRRESSED
+            },
+            child: Text(ttext), //VARIABLE PRINTED USING TEXT
           ),
-        ),
-      ),
-    );
+        )));
   }
 }
